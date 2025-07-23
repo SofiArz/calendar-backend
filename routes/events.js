faea868412fa2aafb1afbab5cbe5194143e2a1a6
Sofia Arzamendia <sofia.arzamendia@hotmail.es>
2025-07-23 16:17:03 +0200

    createEvent request validation

--- routes/events.js
+++ routes/events.js
@@ -1,13 +1,30 @@
 const { Router } = require('express');
 const { validateJWT } = require('../middlewares/jwt-validator');
 const { getEvents, createEvent, updateEvent, deleteEvent } = require('../controllers/events')
+const { check } = require('express-validator');
+const { validateRequestFields } = require('../middlewares/fields-validator')
 
 const router = Router();
 router.use(validateJWT);
 
 router.get('/', getEvents);
 
-router.post('/', createEvent);
+router.post('/', [
+    check('title', 'Title is required.').not().isEmpty(),
+    check('start', 'Start date is required.')
+        .isISO8601()
+        .toDate(),
+    check('end', 'End date is required.')
+        .isISO8601()
+        .toDate()
+        .custom((end, { req }) => {
+            if (new Date(end) <= new Date(req.body.start)) {
+                throw new Error('End date must be after start date.');
+            }
+            return true;
+        }),
+    validateRequestFields
+], createEvent);
 
 router.put('/:id', updateEvent);
 

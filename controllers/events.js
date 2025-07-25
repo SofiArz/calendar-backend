43cda5693643be58b7f03114f2536deed9995eda
Sofia Arzamendia <sofia.arzamendia@hotmail.es>
2025-07-25 19:41:08 +0200

    add updateEvent logic to properly update MongoDB register

--- controllers/events.js
+++ controllers/events.js
@@ -4,16 +4,19 @@ const Event = require('../models/event');
 const getEvents = async (req, res = response) => {
 
     try {
+        const events = await Event.find()
+            .populate('user', 'name');
+
         return res.status(200).json({
             ok: true,
-            msg: 'Get events successful'
-        })
+            events
+        });
     }
     catch (error) {
-        return res.status(500).json()({
+        return res.status(500).json({
             ok: false,
             msg: 'An unexpected error has occurred on our server.'
-        })
+        });
     }
 }
 
@@ -27,29 +30,54 @@ const createEvent = async (req, res = response) => {
         return res.status(201).json({
             ok: true,
             event: savedResult
-        })
+        });
     }
     catch (error) {
         return res.status(500).json({
             ok: false,
             msg: error ?? 'An unexpected error has occurred on our server.'
-        })
+        });
     }
 }
 
 const updateEvent = async (req, res = response) => {
 
     try {
+        const eventId = req.params.id;
+
+        const event = await Event.findById(eventId);
+      
+        if (!event) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'Event not found'
+            });
+        }
+
+        if (event.user.toString() !== req.uid) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'Unauthorized'
+            });
+        }
+
+        const newEvent = {
+            ...req.body,
+            user: req.uid
+        }
+
+        const updatedEvent = await Event.findByIdAndUpdate(eventId, newEvent, { new: true });
+
         return res.status(200).json({
             ok: true,
-            msg: 'Update event successful'
-        })
+            event: updatedEvent
+        });
     }
     catch (error) {
-        return res.status(500).json()({
+        return res.status(500).json({
             ok: false,
             msg: 'An unexpected error has occurred on our server.'
-        })
+        });
     }
 }
 
@@ -59,13 +87,13 @@ const deleteEvent = async (req, res = response) => {
         return res.status(200).json({
             ok: true,
             msg: 'Delete event successful'
-        })
+        });
     }
     catch (error) {
         return res.status(500).json()({
             ok: false,
             msg: 'An unexpected error has occurred on our server.'
-        })
+        });
     }
 }
 
